d3dadf44f941dc0b0d763291024df98eb47bc5d3
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-08-03 13:29:49 +0300

    generate menu-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuFormComponent } from './menu-form/menu-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuFormComponent
   ],
   imports: [
     BrowserModule,
