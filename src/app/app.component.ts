6e037c70ab7c364598dfbb7f33b84bf5a7202b51
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-08-05 16:17:25 +0300

    implement new changes on service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { from } from 'rxjs';
 import { MenuComponent } from './menu/menu.component';
-
+import {GetApiService} from './get-api.service';
 // import { Shop } from './shop';
 
 @Component({
@@ -10,5 +10,27 @@ import { MenuComponent } from './menu/menu.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Call Api';
+  constructor(private api: GetApiService){
+  }
 
+// tslint:disable-next-line:typedef
+// tslint:disable-next-line:use-lifecycle-interface
+ngOnInit(){
+  this.api.apiCall().subscribe((data) => {
+    console.warn('get api data', data);
+    // tslint:disable-next-line:no-string-literal
+    this.title = data['title'];
+  });
 }
+}
+
+
+// tslint:disable-next-line:typedef
+// function ngOnInit() {
+//   throw new Error('Function not implemented.');
+// }
+// function ngOnInit() {
+//   throw new Error('Function not implemented.');
+// }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuFormComponent } from './menu-form/menu-form.component';
@@ -25,7 +25,8 @@ import { ShopFormComponent } from './shop-form/shop-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
