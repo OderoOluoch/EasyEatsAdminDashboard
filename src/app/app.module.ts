e8cbf315f4d450cda268c44a859d5bc8cd5345c6
Serahwakaba <serahwakaba9@gmail.com>
2021-08-09 12:47:46 +0300

    httpclient module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuFormComponent } from './menu-form/menu-form.component';
@@ -11,6 +10,8 @@ import { MenuDetailComponent } from './menu-detail/menu-detail.component';
 import { ShopComponent } from './shop/shop.component';
 import { ShopDetailComponent } from './shop-detail/shop-detail.component';
 import { ShopFormComponent } from './shop-form/shop-form.component';
+import {HttpClientModule} from '@angular/common/http';
+
 // import {GetApiService} from './get-api.service';
 @NgModule({
   declarations: [
