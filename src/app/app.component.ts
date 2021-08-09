f942694c58b7f5e9bc9e3ca7b9156d69d45ab307
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-08-09 15:40:30 +0300

    implement new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,12 @@ import {GetApiService} from './get-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Call Api';
+   Api: any [] = [];
+  GetApiService: any;
   constructor(private api: GetApiService){
   }
 
-ngOnInit(){
-  this.api.apiCall().subscribe((data) => {
-    console.log('get api data', data);
-    // tslint:disable-next-line:no-string-literal
-    this.title = data['title'];
-  });
-}
+
 }
 
-
-// tslint:disable-next-line:typedef
-// function ngOnInit() {
-//   throw new Error('Function not implemented.');
-// }
-// function ngOnInit() {
-//   throw new Error('Function not implemented.');
-// }
 
