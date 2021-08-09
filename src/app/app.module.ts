a69a50c9d0247e8fdb6d5d9e416a120493ff6a48
Serahwakaba <serahwakaba9@gmail.com>
2021-08-09 14:44:42 +0300

    GET request commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuFormComponent } from './menu-form/menu-form.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuDetailComponent } from './menu-detail/menu-detail.component';
 
+
 import { ShopComponent } from './shop/shop.component';
 import { ShopDetailComponent } from './shop-detail/shop-detail.component';
 import { ShopFormComponent } from './shop-form/shop-form.component';
