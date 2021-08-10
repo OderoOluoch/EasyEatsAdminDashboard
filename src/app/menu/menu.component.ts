abb567044aee4cf1aa20175956b53842f921e8c4
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-08-10 23:44:34 +0300

    implement new changes on menu component.ts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GetApiService } from '../get-api.service';
 import { Menu } from '../menu';
 
 @Component({
@@ -7,12 +8,13 @@ import { Menu } from '../menu';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  menus: Menu[] = [
-    new Menu(1, 'BreakFast', 'A healthy start to the day.'),
-    new  Menu(2, 'Lunch', 'Flavorsome Lunch To Keep You Going'),
-    new  Menu(3, 'Soft Drinks', 'enjoy our drinks made of  light and refreshing with all the flavors of a Shirley Temple you love'),
-    new  Menu(4, 'Desserts', 'Bringing you your favorite desserts'),
-  ]
+  // menus: Menu[] = [
+  //   new Menu(1, 'BreakFast', 'A healthy start to the day.'),
+  //   new  Menu(2, 'Lunch', 'Flavorsome Lunch To Keep You Going'),
+  //   new  Menu(3, 'Soft Drinks', 'enjoy our drinks made of  light and refreshing with all the flavors of a Shirley Temple you love'),
+  //   new  Menu(4, 'Desserts', 'Bringing you your favorite desserts'),
+  // ];
+  menus: any;
 
 
     // tslint:disable-next-line:typedef
@@ -33,10 +35,16 @@ export class MenuComponent implements OnInit {
     // shop.name = shop
     this.menus.push(menu);
   }
-  constructor() { }
+  constructor( private getApiService: GetApiService) { }
 
-  // tslint:disable-next-line:typedef
-  ngOnInit() {
+  ngOnInit(): void {
+      this.getApiService.apiCall().subscribe((response: any) => {
+         this.menus =  response;
+         console.log(this.menus);
+
+        // tslint:disable-next-line:no-string-literal
+
+      });
+    }
   }
 
-}
