73d63173f0c9781c705af7f0cb536238f16ffc5b
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-08-10 23:34:51 +0300

    add new Api for menu

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -13,7 +13,13 @@ export class GetApiService {
     return this.http.get('https://easyeatsodero.herokuapp.com/api/v1/shops');
   }
 
+  // tslint:disable-next-line:typedef
   apiCallwaiters(){
     return this.http.get('https://easyeatsodero.herokuapp.com/api/v1/waiters');
   }
+  // tslint:disable-next-line:typedef
+  apiCallmenu(){
+    return this.http.get('https://easyeatsodero.herokuapp.com/api/v1/menus/new');
+  }
 }
+
