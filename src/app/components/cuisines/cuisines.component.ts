73708e74d48e7763ce8ec12e0b668048570323a6
OderoOluoch <oderoluoch@gmail.com>
2021-08-15 14:14:17 +0300

    image upload form and files ready. We can now select an image and refresh to default

--- src/app/components/cuisines/cuisines.component.ts
+++ src/app/components/cuisines/cuisines.component.ts
@@ -18,6 +18,7 @@ export class CuisinesComponent implements OnInit {
   shops: any;
   cuisines: any;
   imgSrc:string = "/assets/img/dif.jpg"
+  selectedImage:any = null;
 
   formTemplate = new FormGroup({
     foodType: new FormControl(''),
@@ -45,6 +46,18 @@ export class CuisinesComponent implements OnInit {
       this.shops = response;
       console.log(this.shops);
     });
+  }
+
+  showPreview(event:any){
+    if(event.target.files && event.target.files[0]){
+      const reader = new FileReader();
+      reader.onload = (e:any) => this.imgSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    }else{
+      this.imgSrc = "/assets/img/dif.jpg";
+      this.selectedImage = null;
+    }
   }
 
   // resetForm(form?: NgForm) {
